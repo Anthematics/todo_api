b778b0e19fc5dce82a995b0660833e2b4db641a4
Anthematics <Jasonharder2@gmail.com>
2019-07-07 16:35:25 -0400

    fixed thermostats to be an array of objects

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
 app.get('/api/v1/thermostats',(req,res) => {
   res.status(200).send({
     success:'true',
@@ -36,6 +35,82 @@ app.get('/api/v1/thermostats/:id', (req,res) => {
   });
 });
 
+app.post('/api/v1/thermostats/:id',(req,res) => {
+  if (!req.body.deviceName) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'no device name specified'
+    });
+    } else if (!req.body.deviceType) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'no device type specified'
+      });
+    } else if (!req.body.modelNumber) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'no model number specified'
+      });
+    } else if (!req.body.productCode) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'No given product code'
+      })
+    } else if (!req.body.serialNumber) {
+      return res.status(400).send({
+        success:'false',
+        message: 'No given serial number'
+      })
+    } else if (!req.body.registered) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'unsure if registered'
+      })
+    } else if (!req.body.room) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'not given a room'
+      })
+    } else if (!req.body.address) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'please provide address'
+      })
+    } else if (!req.body.postalCode) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'no postal code / zip code given'
+      })
+    }
+    const thermostat = {
+      id: db.length +1,
+      deviceName = req.body.deviceName,
+      modelNumber = req.body.modelNumber,
+      productCode = req.body.productCode,
+      serialNumber = req.body.serialNumber,
+      registered = req.body.registered,
+      room = req.body.room,
+      address = req.body.address,
+      postalCode = req.body.postalCode
+    }
+  db.push(thermostat);
+  return res.status(201).send({
+    success: 'true',
+    message: 'thermostat added successfully',
+    thermostat
+  })
+  });
+
+
+// deviceName: "Jason's ecobee the first.",
+// deviceType: "thermostat",
+// modelNumber: "APOLLO_SMART",
+// productCode: "1H",
+// serialNumber: "511898777555",
+// registered: true,
+// room: "Kitchen",
+// address: "235 Acton Avenue",
+// postalCode: "M3H4H9"
 
 
 app.delete('/api/v1/thermostats/:id', (req,res) => {
@@ -56,6 +131,8 @@ db.map((thermostat,index) => {
   })
 
 })
+
+
 
 
 const PORT = 5000
